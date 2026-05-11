15db95a8940f9b5aaba2a73b2f07766129c6eb35
Qin Shi Huang <155852344+blooperson@users.noreply.github.com>
2026-05-10 22:58:55 -0400

    Update Follow mouse.cs

--- Tavern Minigames WIP 2D/Assets/T1 Tavern/T1 Minigames/Bread minigame/Part 2/Scripts/Follow mouse.cs
+++ Tavern Minigames WIP 2D/Assets/T1 Tavern/T1 Minigames/Bread minigame/Part 2/Scripts/Follow mouse.cs
@@ -1,3 +1,5 @@
+// -- UNUSED SCRIPT --
+
 using UnityEngine;
 
 public class Followmouse : MonoBehaviour
@@ -11,6 +13,7 @@ public class Followmouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Retreives the position of the user's mouse and sets the z value to 0 so that the object will be on the same plane as the camera
         Input.mousePosition.Set(Input.mousePosition.x, Input.mousePosition.y, 0);
         transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
