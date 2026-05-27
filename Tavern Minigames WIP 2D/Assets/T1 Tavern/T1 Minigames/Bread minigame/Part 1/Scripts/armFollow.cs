5c16251972dc09607454f01993c97be5955f9d65
Qin Shi Huang <155852344+blooperson@users.noreply.github.com>
2026-05-26 23:48:00 -0400

    Update armFollow.cs

--- Tavern Minigames WIP 2D/Assets/T1 Tavern/T1 Minigames/Bread minigame/Part 1/Scripts/armFollow.cs
+++ Tavern Minigames WIP 2D/Assets/T1 Tavern/T1 Minigames/Bread minigame/Part 1/Scripts/armFollow.cs
@@ -19,12 +19,16 @@ public class armFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // constantly follow the mouse position
         followMouse();
     }
 
     void followMouse()
     {
+        // Intitialize and find the mouse
         Mouse mouse = Mouse.current;
+
+        // find the mouse position and set the z to 0 so it doesn't mess with the target joint, also make the target join follow the mouse
         Input.mousePosition.Set(Input.mousePosition.x, Input.mousePosition.y, 0);
         Tj2d.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
