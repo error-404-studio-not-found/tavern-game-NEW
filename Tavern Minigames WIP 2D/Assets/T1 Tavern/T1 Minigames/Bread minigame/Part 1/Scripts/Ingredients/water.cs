4dc4be085765676fb3c575553dca2bf8e68ba297
Qin Shi Huang <155852344+blooperson@users.noreply.github.com>
2026-05-29 02:53:08 -0400

    Began working on dough watering system
    Still in progress but added basic follow and place function as well as begining layer mask work.

---
+++ Tavern Minigames WIP 2D/Assets/T1 Tavern/T1 Minigames/Bread minigame/Part 1/Scripts/Ingredients/water.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
+
+public class water : MonoBehaviour
+{
+    [Header("Carry Water")]
+    private TargetJoint2D waterJugCarry;
+    private Rigidbody2D Rb;
+    public GameObject hand;
+    private bool holdingWater = false;
+    public GameObject waterPlacement;
+
+    [Header("References")]
+    [SerializeField] private LayerMask targetLayerMask;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        waterJugCarry = GetComponent<TargetJoint2D>();
+        Rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if they should be holding the water
+        waterPickUp();
+
+
+        //Enable the collider and carry if the player is holding the water, otherwise disable it
+        if (holdingWater)
+        {
+            holdWater();
+        }
+        else
+        {
+            waterJugCarry.enabled = false;
+        }
+
+
+    }
+
+    /* 
+     * this function checks if the player is clicking on the water jug, if they are then they will hold the water until they click again, if they click somewhere else then they will not hold the water 
+     */
+    public void waterPickUp()
+    {
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+
+        Mouse mouse = Mouse.current;
+        if (mouse.leftButton.wasPressedThisFrame && holdingWater == false)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mouseScreenPos), Vector2.zero);
+            if (hit.collider != null && hit.collider.name == "WaterJug")
+            {
+                holdingWater = true;
+                waterJugCarry.enabled = true;
+                Rb.bodyType = RigidbodyType2D.Dynamic;
+                Rb.freezeRotation = true;
+            }
+            else
+            {
+                Debug.Log("Did not click water");
+            }
+
+        }
+
+        // Place water down if it is currently being held and the player clicks somewhere else
+        else if (mouse.leftButton.wasPressedThisFrame && holdingWater)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mouseScreenPos), Vector2.zero);
+            if (hit.collider != null && hit.collider.name == "JugPlacement")
+            {
+                placeWater();
+            }
+            else
+            {
+                Debug.Log("Did put water down");
+            }
+
+        }
+
+    }
+
+    public void holdWater()
+    {
+        waterJugCarry.target = hand.transform.position;
+    }
+
+    public void placeWater()
+    {
+        holdingWater = false;
+        waterJugCarry.enabled = false;
+        Rb.bodyType = RigidbodyType2D.Static;
+
+        gameObject.transform.position = waterPlacement.transform.position;
+    }
+
+
+}
